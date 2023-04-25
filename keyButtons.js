9d47762908ac341eeeaf2bcf7584020277fd79f8
ElenaSukhina1 <lenanikitina1@yandex.ru>
2023-04-25 21:06:51 +0300

    init: start virtual-keyboard

---
+++ keyButtons.js
@@ -0,0 +1,92 @@
+const keyButton = [
+    {
+        name: '`',
+        atribute: 'simbol'
+
+    },
+    {
+        name: '1',
+    },
+    {
+        name: '2',
+    },
+    {
+        name: '3',
+    },
+    {
+        name: '4',
+    },
+    {
+        name: '5',
+    },
+    {
+        name: '6',
+    },
+    {
+        name: '7',
+    },
+    {
+        name: '8',
+    },
+    {
+        name: '9',
+    },
+    {
+        name: '0',
+    },
+    {
+        name: '-',
+    },
+    {
+        name: '=',
+    },
+    {
+        name: 'Backspace',
+        atribute: 'special-large'
+
+    },
+    {
+        name: 'Tabe',
+        atribute: 'special-small'
+
+    },
+    {
+        name: 'Q',
+    },
+    {
+        name: 'W',
+    },
+    {
+        name: 'E',
+    },
+    {
+        name: 'R',
+    },
+    {
+        name: 'T',
+    },
+    {
+        name: 'Y',
+    },
+    {
+        name: 'U',
+    },
+    {
+        name: 'I',
+    },
+    {
+        name: 'O',
+    },
+    {
+        name: 'P',
+    },
+    {
+        name: '[',
+    },
+    {
+        name: ']',
+    },
+    {
+        name: '&#8260;',
+    },
+]
