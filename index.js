338d31436a76249362a35e115ce75268e5321bfa
ElenaSukhina1 <lenanikitina1@yandex.ru>
2023-04-27 21:33:26 +0300

    feat: add first block in Keyboard

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+const keybordSection = document.querySelector('.key-section')
+const firstArr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace']
 
 
-let header = document.getElementsByTagName('h1')[0]
-console.log(header)
-header.classList.add("colortext");
+function createKeyBlock (){
+for(let i = 0; i < 5; i++){
+    let keyBlock = document.createElement('div')
+    keybordSection.appendChild(keyBlock)
+    keyBlock.classList.add('key-block')
+    console.log(keyBlock)
+}
+}
 
+createKeyBlock ()
+
+function createKeyButton1 (){
+    firstArr.forEach(value =>{
+        let firstKeyBlock = document.querySelectorAll('.key-block')[0]
+        let keyButton1 = document.createElement('button')
+        firstKeyBlock.appendChild(keyButton1)
+        keyButton1.classList.add('button')
+        keyButton1.textContent = value
+        if(keyButton1.textContent === 'Backspace'){
+            keyButton1.classList.add('backspace')
+        }
+    })
+}
+createKeyButton1 ()
--- keyButtons.js
+++ keyButtons.js
@@ -1,11 +1,11 @@
 class Title {
     constructor(){
         this.elem = document.createElement('h1')
-        this.text = document.createTextNode('RSS Виртуальная клавиатура')
+        this.elem.textContent = 'RSS Виртуальная клавиатура'
+        this.elem.classList.add('colortext')
     }
     apendTo(parent){
         parent.appendChild(this.elem)
-        document.body.childNodes[2].appendChild(this.text)
     }
 
 }
@@ -14,6 +14,7 @@ new Title().apendTo(document.body);
 class Textarea {
     constructor(){
         this.elem = document.createElement('textarea')
+        this.elem.classList.add('textarea-style')
     }
     apendTo(parent){
         parent.appendChild(this.elem)
@@ -25,9 +26,35 @@ new Textarea().apendTo(document.body);
 class Keysection {
     constructor(){
         this.elem = document.createElement('div')
+        this.elem.classList.add('key-section')
     }
     apendTo(parent){
         parent.appendChild(this.elem)
     }
 }
 new Keysection().apendTo(document.body)
+
+class Description {
+    constructor(){
+        this.elem = document.createElement('p')
+        this.elem.classList.add('description')
+        this.elem.textContent = 'Клавиатура создана в операционной системе Windows'
+    }
+    apendTo(parent){
+        parent.appendChild(this.elem)
+    }
+
+}
+new Description().apendTo(document.body)
+
+class Lang {
+    constructor(){
+        this.elem = document.createElement('p')
+        this.elem.classList.add('lang')
+        this.elem.textContent = 'Для переключения языка комбинации: ctrl + alt'
+    }
+    apendTo(parent){
+        parent.appendChild(this.elem)
+    }
+}
+new Lang().apendTo(document.body)
